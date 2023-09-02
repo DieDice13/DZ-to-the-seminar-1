7b90c69965b6d788f6ccbfa3357ee69346065b67
Ruslan <die.dice.2003@mail.ru>
2023-09-02 17:20:34 +0600

    We have done the third task

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 == 0)
+{
+    Console.Write("This number is even : " + num1);
+}
+else 
+{
+    Console.Write("This number is not even : " + num1);
+}
