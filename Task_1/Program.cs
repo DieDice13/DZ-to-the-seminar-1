309385e9c3f77d977db474a56e373ca8519f821f
Ruslan <die.dice.2003@mail.ru>
2023-09-02 16:29:22 +0600

    We did the first task

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter first number : ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number : ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+
+if (max > num2)
+{
+    Console.WriteLine("Max : " + max);
+    Console.Write("Min : " + num2);
+}
+else
+{
+    Console.WriteLine("Max : " + num2);
+    Console.Write("Min : " + max);
+}
