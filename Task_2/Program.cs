8fd07d347a029f82d7b6fb084ee1feed7aa8805a
Ruslan <die.dice.2003@mail.ru>
2023-09-02 17:06:59 +0600

    We did the second task

---
+++ Task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] nums = new int[3];
+Console.Write("Enter first numbers : ");
+nums[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second numbers : ");
+nums[1] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter third numbers : ");
+nums[2] = Convert.ToInt32(Console.ReadLine());
+int max = nums[0];
+
+Console.WriteLine("Работаем со следующим массивом ");
+var str = string.Join(" ", nums);
+Console.WriteLine(str);
+
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[1] > max)
+    {
+        max = nums[1];
+    }
+    else  
+    {
+        if (nums[2] > max)
+        max = nums[2];
+    }  
+}
+Console.WriteLine("The maximum of these numbers : " + max);
+
+  
+
+
